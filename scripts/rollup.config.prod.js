23f4aa273eb77de58c38a6d34b3179cf50ead8fb
Sfl <942368681@qq.com>
2020-03-26 09:39:02 +0800

    update

--- scripts/rollup.config.prod.js
+++ scripts/rollup.config.prod.js
@@ -15,7 +15,7 @@ export default [
         output: {
             name,
             banner,
-            file: `dist/${name}.js`,
+            file: `dist/${name}.min.js`,
             format: 'umd'
         },
         plugins: [
